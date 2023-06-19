af46caa1b10832e9285123d16b7007b47874ef28
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-06-19 20:56:22 +0300

    create posts. Add styles.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import {Counter} from "./components/Counter.jsx";
+import React, {useState} from 'react';
+import './styles/App.css';
 
 function App() {
+  const [value, setValue] = useState("Текст в инпуте");
+
   return (
     <div className="App">
-      <Counter/>
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div>Javascript - язык программирования.</div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="post__content">
+          <strong>2. Java</strong>
+          <div>Java - ООП язык программирования общего назначения.</div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="post__content">
+          <strong>3. PHP</strong>
+          <div>PHP - скриптовый серверный язык программирования.</div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
     </div>
   )
 }
