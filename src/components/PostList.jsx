3e02af5c4ddb036520ff4afe3835a21a357c0b77
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-06-20 13:10:10 +0300

    fix. posts title make align center.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,7 +4,7 @@ import {PostItem} from "./PostItem";
 export const PostList = ({posts, title, remove}) => {
   return (
     <div>
-      <h1>{title}:</h1>
+      <h1 style={{textAlign: 'center'}}>{title}:</h1>
       {posts.map((post, index) =>
         <PostItem remove={remove} number={index + 1} post={post} key={Date.now() + index}/>
       )}
