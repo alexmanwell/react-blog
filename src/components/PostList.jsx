d670fd2fb404c66988aee0c640ab583a31bc3c06
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-06-20 16:15:15 +0300

    add animated for create and remove post.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {PostItem} from "./PostItem";
+import TransitionGroup from "react-transition-group/esm/TransitionGroup";
+import CSSTransition from "react-transition-group/esm/CSSTransition";
 
 export const PostList = ({posts, title, remove}) => {
   if (!posts.length) {
@@ -11,9 +13,17 @@ export const PostList = ({posts, title, remove}) => {
   return (
     <div>
       <h1 style={{textAlign: 'center'}}>{title}:</h1>
-      {posts.map((post, index) =>
-        <PostItem remove={remove} number={index + 1} post={post} key={Date.now() + index}/>
-      )}
+      <TransitionGroup>
+        {posts.map((post, index) =>
+          <CSSTransition
+            key={post.id}
+            timeout={300}
+            classNames="post"
+          >
+            <PostItem remove={remove} number={index + 1} post={post} key={Date.now() + index}/>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </div>
   );
 };
