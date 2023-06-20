625863c311c7ca9282e2cc0c6bc3e10fc15cd816
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-06-20 18:26:42 +0300

    implement loader(rotate circle) component for waiting loading posts data

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import MyModal from "./components/UI/modal/MyModal";
 import {MyButton} from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
 import PostService from "./api/PostService";
+import {Loader} from "./components/UI/loader/Loader";
 
 function App() {
   const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
   const fetchPosts = async () => {
+    setIsPostsLoading(true);
     const posts = await PostService.getAll();
     setPosts(posts);
+    setIsPostsLoading(false);
   };
 
   useEffect(() => {
@@ -46,7 +50,14 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Post List #1"}/>
+      {isPostsLoading
+        ?
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
+          <Loader/>
+        </div>
+        :
+        <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Post List #1"}/>
+      }
     </div>
   )
 }
