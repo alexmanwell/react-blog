6f4ba69f07ebb05e9d3429df0c0c22c5a43a50c2
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-06-21 12:41:47 +0300

    create navbar component consisting of 2 links (about and posts).

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/App.css';
-import {Route, BrowserRouter, Routes} from "react-router-dom";
+import {Route, BrowserRouter, Routes, Link} from "react-router-dom";
 import About from "./components/pages/About";
 import {Posts} from "./components/pages/Posts";
+import {Navbar} from "./components/UI/navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/about" element={<About/>}/>
         <Route path="/posts" element={<Posts/>}/>
