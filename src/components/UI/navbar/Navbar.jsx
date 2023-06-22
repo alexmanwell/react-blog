6626ceb7deebc1f202c0690b19c664cdff8692be
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-06-22 03:18:23 +0300

    implement sign out.

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link} from "react-router-dom";
+import {MyButton} from "../button/MyButton";
+import {AuthContext} from "../../../context";
 
 export const Navbar = () => {
+  const {isAuth, setIsAuth} = useContext(AuthContext);
+
   return (
     <div className="navbar">
+      <MyButton onClick={() => setIsAuth(false)}>Sign Out</MyButton>
       <div className="navbar__links">
         <Link to="/about">About</Link>
         <Link to="/posts">Posts</Link>
